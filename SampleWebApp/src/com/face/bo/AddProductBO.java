185b04788dd9f3dc42eb2edd4c390f45a2bfb4d9
Rosna Simon <51952002+rosnasimon@users.noreply.github.com>
2019-11-27 12:54:26 +0530

    Add files via upload

---
+++ SampleWebApp/src/com/face/bo/AddProductBO.java
@@ -0,0 +1,35 @@
+package com.face.bo;
+
+public class AddProductBO {
+
+	private int pid;
+	private String pname;
+	private int price;
+	private int quantity;
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+}
